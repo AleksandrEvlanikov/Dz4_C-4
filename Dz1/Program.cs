d31d68cdf74dfce8d44f7dabcbf1a3032fcc70b7
Aleksandr <aleksandr.evlanicov@gmail.com>
2022-12-12 15:38:22 +0300

    Dz1

---
+++ Dz1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число A ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+int result = 1;
+  for (int i = 1; i <= number1; i++)
+    result = result * number;
+
+Console.WriteLine(result);
+
