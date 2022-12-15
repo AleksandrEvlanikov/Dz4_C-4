f99d6a230ee6eda6926c1e36c17dfac0c91aacfe
Aleksandr <aleksandr.evlanicov@gmail.com>
2022-12-15 12:26:44 +0300

    Dz2

---
+++ Dz2/Program.cs
@@ -0,0 +1,18 @@
+﻿
+   Console.WriteLine("Введите число ");
+   int N = Convert.ToInt32(Console.ReadLine());
+   
+   int i = 1;
+   while (i <= N)
+   {
+    if (N % i == 0)
+    Console.WriteLine(i);
+     i++;
+   }
+   
+
+
+  
+
+ 
+
